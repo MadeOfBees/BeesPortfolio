a0615febfe7e23c0f4190d5b558c037e5458d358
Bee <xronsredstone@gmail.com>
2022-11-15 21:44:37 -0500

    base code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Form from './components/Form/index';
+import Nav from './components/Nav/index';
 
 function App() {
-  return <Form />;
+  return <Nav />;
 }
 
 export default App;
