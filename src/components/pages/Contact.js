89aaabde86d98c7c3125c6fc51f5a8ea3b34bdd5
Bee <xronsredstone@gmail.com>
2022-11-21 22:24:20 -0500

    contact shell

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
 
 
 export default function Contact() {
@@ -6,9 +7,21 @@ export default function Contact() {
     <div>
       <h1>Contact Bee:</h1>
       <ul>
-        <li>Github: MadeOfBees</li>
-        <li>Email: xronsredstone@gmail.com</li>
-        <li>Linkdin: None yet</li>
+        <li><div className='row'>
+          <p className='col'> Email</p>
+          <Form className='col'><input></input></Form>
+          <div className='col'></div>
+        </div></li>
+        <li><div className='row'>
+          <p className='col'> Name</p>
+          <Form className='col'><input></input></Form>
+          <div className='col'></div>
+        </div></li>
+        <li>
+          <p> Message</p>
+          <Form><input></input></Form>
+          <div className='col'></div>
+</li>
       </ul>
     </div>
   );
