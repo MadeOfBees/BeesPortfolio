679ad2b4a4f38c4534d08248ecce6257d93aad62
Bee <xronsredstone@gmail.com>
2022-11-21 21:11:55 -0500

    badges

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { MdOutlineMail } from 'react-icons/md';
 
 export default function Home() {
   return (
     <div>
       <h1>About Bee:</h1>
-      <ul>
-        <li>
+      <p>
         Hi, i'm Bee and this is my portfolio, I don't really know what to write here so here's some sample text.
-        </li>
-        <li>Github: MadeOfBees</li>
-        <li>Email: xronsredstone@gmail.com</li>
-        <li>Linkdin: None yet</li>
-      </ul>
-    </div>
+      </p>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col">
+          <a href="https://github.com/MadeOfBees"><BsGithub />Github: MadeOfBees</a>
+          </div>
+          <div class="col">
+            <a href="mailto: xronsredstone@gmail.com"><MdOutlineMail />Email: xronsredstone@gmail.com</a>
+          </div>
+          <div class="col">
+            <BsLinkedin />
+            Linkdin: None yet
+          </div>
+        </div>
+      </div>
+    </div >
   );
 }
--- src/components/pages/ProgCell.js
+++ src/components/pages/ProgCell.js
@@ -18,7 +18,7 @@ export default function ProjCell({ currentPage, handlePageChange }) {
           src="https://user-images.githubusercontent.com/9198297/201371225-a8b36224-6e2c-43a3-b66f-da3140a7a096.jpg"
           alt="SQL Store"
           onClick={() => handlePageChange('Proj1')}
-          className={`d-block w-100 ${page1}`}
+          className={`d-block w-100 h-30 ${page1}`}
         />
         <Carousel.Caption>
           <h5>SQL Store</h5>
@@ -27,7 +27,7 @@ export default function ProjCell({ currentPage, handlePageChange }) {
       <Carousel.Item>
         <Image
           onClick={() => handlePageChange('Proj2')}
-          className={`d-block w-100 ${page2}`}
+          className={`d-block w-100 h-30 ${page2}`}
           src="https://user-images.githubusercontent.com/9198297/190828203-2adb4546-0e32-43f5-a163-07e28f079d48.png"
           alt="PasswordGen"
         />
@@ -38,7 +38,7 @@ export default function ProjCell({ currentPage, handlePageChange }) {
       <Carousel.Item>
         <Image
           onClick={() => handlePageChange('Proj3')}
-          className={`d-block w-100 ${page3}`}
+          className={`d-block w-100 h-30 ${page3}`}
           src="https://i.redd.it/i0h9ke187tk31.png"
           alt="Readme generator"
         />
@@ -49,7 +49,7 @@ export default function ProjCell({ currentPage, handlePageChange }) {
       <Carousel.Item>
         <Image
           onClick={() => handlePageChange('Proj4')}
-          className={`d-block w-100 ${page4}`}
+          className={`d-block w-100 h-30 ${page4}`}
           src="https://user-images.githubusercontent.com/9198297/197867296-86436430-632b-4173-83d9-51cb3783512b.png"
           alt="Weather app"
         />
@@ -60,7 +60,7 @@ export default function ProjCell({ currentPage, handlePageChange }) {
       <Carousel.Item>
         <Image
           onClick={() => handlePageChange('Proj5')}
-          className={`d-block w-100 ${page5}`}
+          className={`d-block w-100 h-30 ${page5}`}
           src="https://i.redd.it/i0h9ke187tk31.png"
           alt="First slide"
         />
@@ -71,7 +71,7 @@ export default function ProjCell({ currentPage, handlePageChange }) {
       <Carousel.Item>
         <Image
           onClick={() => handlePageChange('Prog6')}
-          className={`d-block w-100 ${page6}`}
+          className={`d-block w-100 h-30 ${page6}`}
           src="https://user-images.githubusercontent.com/9198297/190828201-4c425bc6-afbb-478d-9ced-a84e829e34c3.png"
           alt="Fibonacci generator"
         />
@@ -82,7 +82,7 @@ export default function ProjCell({ currentPage, handlePageChange }) {
       <Carousel.Item>
         <Image
           onClick={() => handlePageChange('Proj7')}
-          className={`d-block w-100 ${page7}`}
+          className={`d-block w-100 h-30 ${page7}`}
           src="https://user-images.githubusercontent.com/9198297/200918630-01c4f4fd-d28e-4ce9-99c0-1ba52a8e63f4.jpg"
           alt="Team Maker"
         />
