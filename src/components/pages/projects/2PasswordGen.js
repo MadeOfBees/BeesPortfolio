54b801d3328ea5e81bcf23b98bfa731c129d5295
Bee <xronsredstone@gmail.com>
2022-11-23 11:35:04 -0500

    weird bug but okay

--- src/components/pages/projects/2PasswordGen.js
+++ src/components/pages/projects/2PasswordGen.js
@@ -16,7 +16,7 @@ export default function Proj2() {
       <h1>PasswordGen</h1>
       <ul>
         <li>A simple password generator</li>
-        <li><a href="https://madeofbees.github.io/passwordGen/">https://madeofbees.github.io/passwordGen/</a></li>
+        <li><a href="https://bit.ly/3XqBH07">https://madeofbees.github.io/passwordGen/</a></li>
         <li><a href="https://github.com/MadeOfBees/passwordGen">https://github.com/MadeOfBees/passwordGen</a></li>
       </ul>
     </div>
